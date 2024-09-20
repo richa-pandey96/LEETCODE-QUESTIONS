9fa725120e5445d48e9a1b17c840e4354302f7a0
Richa Pandey <123984046+richa-pandey96@users.noreply.github.com>
2024-09-20 22:07:37 +0530

    Time: 6 ms (68.64%), Space: 11.5 MB (68.42%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        string temp = s + "#" + rev;
+
+        vector<int> lps(temp.size(), 0);
+        for (int i = 1; i < temp.size(); i++) {
+            int j = lps[i - 1];
+            while (j > 0 && temp[i] != temp[j])
+                j = lps[j - 1];
+            if (temp[i] == temp[j])
+                j++;
+            lps[i] = j;
+        }
+
+        string suffix = rev.substr(0, s.size() - lps.back());
+        return suffix + s;
+    }
+};

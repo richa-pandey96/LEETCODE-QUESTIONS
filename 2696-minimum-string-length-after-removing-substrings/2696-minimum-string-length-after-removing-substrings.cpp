a53d1d256f67358283474bfa93ad4e3fddfd1a98
Richa Pandey <123984046+richa-pandey96@users.noreply.github.com>
2024-10-07 18:59:36 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (99.28%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
+    //optimal solution
     int minLength(string s) {
         
-        stack<char> st;
+          int n=s.length();
         
-        for(char c: s){
-            if(!st.empty() && ((st.top()=='A' && c=='B') ||(st.top()=='C' && c=='D'))){
-                st.pop();//remove the pair by popping the stack
-            }else{
-                st.push(c);
+        int i=0;//write
+        int j=1;//read
+        
+        while(j<n){
+            if(i<0){
+                i++;
+                s[i]=s[j];
+                
             }
+            else if((s[i]=='A' && s[j]=='B')||
+                   (s[i]=='C' && s[j]=='D')){
+                i--;
+            }else{
+                i++;
+                s[i]=s[j];
+            }
+            
+            j++;
         }
-        return st.size();
+        return i+1;
     }
 };
+//better approach
+// int minLength(string s) {
+        
+//         stack<char> st;
+        
+//         for(char c: s){
+//             if(!st.empty() && ((st.top()=='A' && c=='B') ||(st.top()=='C' && c=='D'))){
+//                 st.pop();//remove the pair by popping the stack
+//             }else{
+//                 st.push(c);
+//             }
+//         }
+//         return st.size();
+//     }

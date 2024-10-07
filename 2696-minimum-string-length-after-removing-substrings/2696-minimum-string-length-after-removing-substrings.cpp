ea79c1f38cc192ea5023bdad61ce20951b0f45fb
Richa Pandey <123984046+richa-pandey96@users.noreply.github.com>
2024-10-07 18:14:53 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (99.28%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minLength(string s) {
+        
+        int n=s.length();
+        
+        int i=0;//write
+        int j=1;//read
+        
+        while(j<n){
+            if(i<0){
+                i++;
+                s[i]=s[j];
+                
+            }
+            else if((s[i]=='A' && s[j]=='B')||
+                   (s[i]=='C' && s[j]=='D')){
+                i--;
+            }else{
+                i++;
+                s[i]=s[j];
+            }
+            
+            j++;
+        }
+        return i+1;
+        
+    }
+};

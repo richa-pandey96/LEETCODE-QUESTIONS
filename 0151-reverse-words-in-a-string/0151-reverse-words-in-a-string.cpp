975bf46661d628e8f28cc82d3422968bb9ae4de7
Richa Pandey <123984046+richa-pandey96@users.noreply.github.com>
2024-11-07 21:25:57 +0530

    Time: 18 ms (7.60%), Space: 49.5 MB (5.07%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,35 +1,65 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+        
+//         //reverse whole string
+//         reverse(s.begin(),s.end());
+        
+//         int i=0;
+        
+//         //use l ,r for reversing words
+//         int l=0,r=0;
+        
+//         int n=s.length();
+        
+//         while(i<n){
+//             while(i<n && s[i]!=' '){// if i gets a char it will give to r then i++ and r++
+//                 s[r++]=s[i++];
+                
+                
+//             }
+//             if(l<r){//reverse l to r
+//                 reverse(s.begin()+l,s.begin()+r);
+                
+//                 s[r]=' ';
+//                 r++;
+//                 l=r;
+//             }
+//             i++;//i will keep increasing
+//         }
+//         s=s.substr(0,r-1);
+        
+//         return s;
+        
+//     }
+// };
 class Solution {
 public:
     string reverseWords(string s) {
-        
-        //reverse whole string
-        reverse(s.begin(),s.end());
-        
-        int i=0;
-        
-        //use l ,r for reversing words
-        int l=0,r=0;
-        
-        int n=s.length();
-        
-        while(i<n){
-            while(i<n && s[i]!=' '){// if i gets a char it will give to r then i++ and r++
-                s[r++]=s[i++];
-                
-                
+        stack<string> st;
+        int n= s.size();
+        int j=0;
+        while(j<n){
+            if(s[j]==' '){
+                j++;
+                continue;
             }
-            if(l<r){//reverse l to r
-                reverse(s.begin()+l,s.begin()+r);
-                
-                s[r]=' ';
-                r++;
-                l=r;
+            
+            string t="";
+            while(s[j]!=' ' and j<n){
+                t+=s[j];
+                j++;
             }
-            i++;//i will keep increasing
+            st.push(t);
+            
         }
-        s=s.substr(0,r-1);
-        
-        return s;
-        
+        string ans=st.top();
+        st.pop();
+
+        while(!st.empty()){
+            ans=ans+" "+st.top();
+            st.pop();
+        }
+        return ans;
     }
 };
